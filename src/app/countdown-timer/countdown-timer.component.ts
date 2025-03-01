9c76ab0a73c8c475cad3d66b0b3dfbf6e379e5b5
Sohaila-Ali706 <asohila664@gmail.com>
2025-03-01 07:06:08 +0200

    ubdate-countdown

--- src/app/countdown-timer/countdown-timer.component.ts
+++ src/app/countdown-timer/countdown-timer.component.ts
@@ -4,14 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-countdown-timer',
   standalone: false,
   templateUrl: './countdown-timer.component.html',
-  styleUrl: './countdown-timer.component.css'
+  styleUrl: './countdown-timer.component.css',
 })
 export class CountdownTimerComponent {
   days: number = 0;
   hours: number = 0;
   minutes: number = 0;
   seconds: number = 0;
-  targetDate: Date = new Date('2025-03-01T00:00:00'); // غيري التاريخ حسب الحاجة
+  targetDate: Date = new Date('2025-03-01T00:00:00');
 
   ngOnInit() {
     this.updateCounter();
@@ -22,18 +22,22 @@ export class CountdownTimerComponent {
 
   updateCounter() {
     const now = new Date().getTime();
-    const timeLeft = this.targetDate.getTime() - now;
+    let timeLeft = this.targetDate.getTime() - now;
+
+    if (timeLeft <= 0) {
+      this.resetCounter();
+      return;
+    }
 
     this.days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    this.hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.hours = Math.floor(
+      (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     this.minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     this.seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-}
-
-
-
-
- 
-  
   }
 
+  resetCounter() {
+    this.targetDate = new Date(new Date().getTime() + 200 * 60 * 60 * 1000);
+  }
+}
